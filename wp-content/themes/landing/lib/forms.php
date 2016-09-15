4aab44c7a9f14d766672b5fca06df3791ac6afcf
Alisa Herr <alisa@unitymakes.us>
2016-09-15 12:06:01 -0400

    Fix user creation

--- wp-content/themes/landing/lib/forms.php
+++ wp-content/themes/landing/lib/forms.php
@@ -10,7 +10,7 @@ add_filter('caldera_preprocess_live', function($data){
   global $domain;
 
   // Get Precinct ID
-  $school = get_page_by_path($data['school_name'], OBJECT, 'precinct');
+  $school = get_page_by_path(sanitize_title($data['school_name']), OBJECT, 'precinct');
 
   // Create new precinct site based on school code
   $school_id = wpmu_create_blog($domain, '/nc-' . $school->ID . '/', $school->post_title, 1, ['current_theme' => 'Precinct', 'template' => 'precinct', 'stylesheet' => 'precinct']);
@@ -18,44 +18,42 @@ add_filter('caldera_preprocess_live', function($data){
   if (is_wp_error($school_id)) {
     // If this school is already registered, set role to contributor.
     $role = 'contributor';
+    $school_id = get_id_from_blogname('/nc-' . $school->ID);
   } else {
-
-    // Generate the password so that the subscriber will have to check email...
-    $password = wp_generate_password( 12, false );
-
-    $userdata = array(
-      'user_login'  =>  $data['email_address'],
-      'user_email'  =>  $data['email_address'],
-      'user_pass'   =>  $password,
-      'first_name'  =>  $data['first_name'],
-      'last_name'   =>  $data['last_name']
-    );
-
     $role = 'editor';
+  }
 
-    // Create new account for user
-    $user_id = wp_insert_user( $userdata );
+  // Generate the password so that the subscriber will have to check email...
+  $password = wp_generate_password( 12, false );
 
-    if ( ! is_wp_error( $user_id ) ) {
-      // Add custom user meta
-      add_user_meta( $user_id, 'classes', $data['what_do_you_teach'], true );
+  $userdata = array(
+    'user_login'  =>  $data['email_address'],
+    'user_email'  =>  $data['email_address'],
+    'user_pass'   =>  $password,
+    'first_name'  =>  $data['first_name'],
+    'last_name'   =>  $data['last_name']
+  );
 
-      // Move user to correct precinct
-      remove_user_from_blog($user_id, get_current_site()->blog_id); // remove user from main blog.
-      add_user_to_blog( $school_id, $user_id, $role );
-      update_user_meta( $user_id, 'primary_blog', $school_id );
+  // Create new account for user
+  $user_id = wp_insert_user( $userdata );
 
-      // Send user email
-      wp_new_user_notification( $user_id, $password );
+  if ( ! is_wp_error( $user_id ) ) {
+    // Add custom user meta
+    add_user_meta( $user_id, 'classes', $data['what_do_you_teach'], true );
 
-    } else {
-      // If user already registered, delete blog that was created in step 1 and return error message
-      wpmu_delete_blog($school_id, TRUE);
+    // Move user to correct precinct
+    remove_user_from_blog($user_id, get_current_site()->blog_id); // remove user from main blog.
+    add_user_to_blog( $school_id, $user_id, $role );
+    update_user_meta( $user_id, 'primary_blog', $school_id );
 
-      return array(
-        'type' => 'error',
-        'note'	=> $user_id->get_error_message()
-      );
-    }
+    // Send user email
+    wp_new_user_notification( $user_id, $password );
+
+  } else {
+    // If user already registered, return error message
+    return array(
+      'type' => 'error',
+      'note'	=> $user_id->get_error_message()
+    );
   }
 });
