3656ddc9775c2e78227aa66387d52a75818bb849
Alisa Herr <alisa@unitymakes.us>
2016-11-08 22:03:31 -0500

    Alphabetical sort of precincts

--- wp-content/themes/precinct/templates/layouts/results-precincts.php
+++ wp-content/themes/precinct/templates/layouts/results-precincts.php
@@ -5,6 +5,27 @@ $results = json_decode(file_get_contents($uploads . '/election_results.json'), t
 $blog_ids = array_column($results, 'blog_id');
 $blog_ids_unique = array_unique($blog_ids);
 
+// Get all sites
+$sites = array();
+foreach ($blog_ids_unique as $blog_id) {
+  $details = get_blog_details($blog_id);
+  switch_to_blog($blog_id);
+    $q = new WP_Query(['posts_per_page' => 1, 'post_type' => 'election']);
+    if($q->have_posts()): while($q->have_posts()): $q->the_post();
+      if ($details->blogname !== 'North Carolina') {
+        $sanitized = sanitize_title($details->blogname);
+        $sites[$sanitized] = array(
+          'name' => $details->blogname,
+          'link' => get_the_permalink(),
+          'count' => count(array_keys($blog_ids, $blog_id))
+        );
+      }
+    endwhile; endif; wp_reset_postdata();
+  restore_current_blog();
+}
+
+// Sort alphabetically
+ksort($sites);
 ?>
 <div class="panel">
   <div class="panel-heading"><h2 class="h3">Map of participating schools</h2></div>
@@ -21,25 +42,17 @@ $blog_ids_unique = array_unique($blog_ids);
     <table class="table sortable">
       <thead>
         <tr>
-          <th scope="col">Precinct</th>
+          <th scope="col" id="sort-init">Precinct</th>
           <th scope="col">Votes</th>
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($blog_ids_unique as $blog_id) {
-          $details = get_blog_details($blog_id);
-          switch_to_blog($blog_id);
-            $q = new WP_Query(['posts_per_page' => 1, 'post_type' => 'election']);
-            if($q->have_posts()): while($q->have_posts()): $q->the_post();
-              if ($details->blogname !== 'North Carolina') { ?>
-                <tr>
-                  <td><a href="<?php the_permalink(); ?>" target="_blank"><?php echo $details->blogname; ?></a></td>
-                  <td><?php echo count(array_keys($blog_ids, $blog_id)); ?> </td>
-                </tr>
-              <?php }
-            endwhile; endif; wp_reset_postdata();
-          restore_current_blog();
-        } ?>
+        <?php foreach ($sites as $site) { ?>
+          <tr>
+            <td><a href="<?php echo $site['link']; ?>" target="_blank"><?php echo $site['name']; ?></a></td>
+            <td><?php echo $site['count']; ?> </td>
+          </tr>
+        <?php } ?>
       </tbody>
     </table>
   </div>
